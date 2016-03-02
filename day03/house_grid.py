6e54b91b6ebfca060239ed1caf66d247307394a3
Mark Atkin <matkin@windrock.com>
2016-03-02 11:38:59 -0500

    Add Robo-Santa and associated unit tests

--- day03/house_grid.py
+++ day03/house_grid.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 import fileinput
+from enum import Enum
+
+class Move(Enum):
+    SANTA = 0
+    ROBOT = 1
 
 def count_presents(directions):
     x = 0
@@ -10,6 +15,24 @@ def count_presents(directions):
     for move in directions:
         x, y = next_house([x, y], move)
         grid.add((x, y))
+    return len(grid)
+
+def count_presents_with_robot(directions):
+    santa_x = 0
+    santa_y = 0
+    robot_x = 0
+    robot_y = 0
+    houses_delivered = 1
+    grid = { (0, 0) }
+    turn = Move.SANTA
+    for move in directions:
+        if turn == Move.SANTA:
+            santa_x, santa_y = next_house([santa_x, santa_y], move)
+            grid.add((santa_x, santa_y))
+        else:
+            robot_x, robot_y = next_house([robot_x, robot_y], move)
+            grid.add((robot_x, robot_y))
+        turn = Move(1 - turn.value)
     return len(grid)
 
 def next_house(coords, direction):
@@ -27,4 +50,5 @@ def next_house(coords, direction):
 if __name__ == '__main__':
     for line in fileinput.input():
         print 'Delivered to %i houses' % count_presents(line)
+        print 'Delivered to %i houses using Robo-Santa' % count_presents_with_robot(line)
 
--- day03/tests.py
+++ day03/tests.py
@@ -2,7 +2,7 @@
 
 import unittest
 
-from house_grid import count_presents
+from house_grid import count_presents, count_presents_with_robot
 
 class TestDay03(unittest.TestCase):
     def test_count_presents(self):
@@ -10,6 +10,12 @@ class TestDay03(unittest.TestCase):
         self.assertEqual(count_presents('^>v<'), 4)
         self.assertEqual(count_presents('^v^v^v^v^v'), 2)
         self.assertEqual(count_presents(''), 1)
+
+    def test_count_presents_with_robot(self):
+        self.assertEqual(count_presents_with_robot('^v'), 3)
+        self.assertEqual(count_presents_with_robot('^>v<'), 3)
+        self.assertEqual(count_presents_with_robot('^v^v^v^v^v'), 11)
+        self.assertEqual(count_presents_with_robot(''), 1)
 
 if __name__ == '__main__':
     unittest.main()
