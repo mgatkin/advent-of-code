c5786533cc3bbb1a9c95239fb898599869d8b3dd
Mark Atkin <matkin@windrock.com>
2016-03-10 12:38:11 -0500

    Refactor
    Split real input and example input for speed

--- day04/adventcoins.py
+++ day04/adventcoins.py
@@ -7,20 +7,20 @@ from time import time
 def advent_hash(s):
     return md5(s).hexdigest()
 
-def mine_advent_coin(s, stars):
+def mine_advent_coin(key, zeroes):
     number = 0
-    while advent_hash(s + str(number))[:stars] != '0' * stars:
+    while advent_hash(key + str(number))[:zeroes] != '0' * zeroes:
         number += 1
     return number
 
 if __name__ == '__main__':
     for line in fileinput.input():
-        secret_key = line.strip()
-        for number_of_zeroes in 5, 6:
+        key = line.strip()
+        for zeroes in 5, 6:
             start_time = time()
-            number = str(mine_advent_coin(secret_key, number_of_zeroes))
-            hash_of_number = advent_hash(secret_key + number)
-            print 'MD5 hash of ' + secret_key + number + ' starts with ' + \
-                hash_of_number[:(number_of_zeroes + 6)] + \
+            number = str(mine_advent_coin(key, zeroes))
+            hash_result = advent_hash(key + number)
+            print 'MD5 hash of ' + key + number + ' starts with ' + \
+                hash_result[:(zeroes + 6)] + \
                 '... (Ran in %.3f seconds)' % (time() - start_time)
 
