023baf2e53b7bdc44412d9e702b4f8550e6fd6c7
Mark Atkin <matkin@windrock.com>
2016-03-10 10:39:07 -0500

    Add mine_advent_coin function and unit tests

--- day04/adventcoins.py
+++ day04/adventcoins.py
@@ -6,7 +6,15 @@ from md5 import md5
 def advent_hash(s):
     return md5(s).hexdigest()
 
+def mine_advent_coin(s):
+    number = 0
+    while advent_hash(s + str(number))[:5] != '00000':
+        number += 1
+    return number
+
 if __name__ == '__main__':
     for line in fileinput.input():
-        print line
+        number = str(mine_advent_coin(line))
+        hash_of_number = advent_hash(line + number)
+        print 'MD5 hash of' + line.strip() + number + 'starts with ' + hash_of_number[:11] + '...'
 
--- day04/tests.py
+++ day04/tests.py
@@ -2,12 +2,16 @@
 
 import unittest
 
-from adventcoins import advent_hash
+from adventcoins import advent_hash, mine_advent_coin
 
 class TestAdventCoins(unittest.TestCase):
     def test_advent_hash(self):
         self.assertEqual(advent_hash('abcdef609043')[:5], '00000')
         self.assertEqual(advent_hash('pqrstuv1048970')[:5], '00000')
+
+    def test_mine_advent_coin(self):
+        self.assertEqual(mine_advent_coin('abcdef'), 609043)
+        self.assertEqual(mine_advent_coin('pqrstuv'), 1048970)
 
 if __name__ == '__main__':
     unittest.main()
