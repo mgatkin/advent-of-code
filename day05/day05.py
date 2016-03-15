e4571f6555b76fe283742cbb4f622326ff839ec9
Mark Atkin <matkin@windrock.com>
2016-03-15 11:58:03 -0400

    Finished part1

--- day05/day05.py
+++ day05/day05.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
 
 import fileinput
+import re
 
 def nicestring(s):
-    return s
+    re1 = re.compile(r'.*[aeiou].*[aeiou].*[aeiou]')
+    re2 = re.compile(r'.*([a-z])\1')
+    re3 = re.compile(r'.*((ab)|(cd)|(pq)|(xy))')
+    if re1.match(s) is not None and re2.match(s) is not None and re3.match(s) is None:
+        return 'nice'
+    else:
+        return 'naughty'
+
 
 if __name__ == '__main__':
+    nice_lines = 0
     for line in fileinput.input():
-        print line
+        if nicestring(line) == 'nice':
+            nice_lines += 1
+    print fileinput.filename(), 'has', nice_lines, 'nice lines'
 
--- day05/tests.py
+++ day05/tests.py
@@ -6,11 +6,11 @@ from day05 import nicestring
 
 class TestFunction(unittest.TestCase):
     def test_nicestring(self):
-        self.assertEqual(nicestring('ugknbfddgicrmopn', 'nice')
-        self.assertEqual(nicestring('aaa', 'nice')
-        self.assertEqual(nicestring('jchzalrnumimnmhp', 'naughty')
-        self.assertEqual(nicestring('haegwjzuvuyypxyu', 'naughty')
-        self.assertEqual(nicestring('dvszwmarrgswjxmb', 'naughty')
+        self.assertEqual(nicestring('ugknbfddgicrmopn'), 'nice')
+        self.assertEqual(nicestring('aaa'), 'nice')
+        self.assertEqual(nicestring('jchzalrnumimnmhp'), 'naughty')
+        self.assertEqual(nicestring('haegwjzuvuyypxyu'), 'naughty')
+        self.assertEqual(nicestring('dvszwmarrgswjxmb'), 'naughty')
 
 
 if __name__ == '__main__':
