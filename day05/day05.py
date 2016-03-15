80a2d02f7244250b0b6e3b35ffaba80f8876eea7
Mark Atkin <matkin@windrock.com>
2016-03-15 11:41:29 -0400

    Skeleton day05 project with unit tests.

---
+++ day05/day05.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import fileinput
+
+def nicestring(s):
+    return s
+
+if __name__ == '__main__':
+    for line in fileinput.input():
+        print line
+
---
+++ day05/tests.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import unittest
+
+from day05 import nicestring
+
+class TestFunction(unittest.TestCase):
+    def test_nicestring(self):
+        self.assertEqual(nicestring('ugknbfddgicrmopn', 'nice')
+        self.assertEqual(nicestring('aaa', 'nice')
+        self.assertEqual(nicestring('jchzalrnumimnmhp', 'naughty')
+        self.assertEqual(nicestring('haegwjzuvuyypxyu', 'naughty')
+        self.assertEqual(nicestring('dvszwmarrgswjxmb', 'naughty')
+
+
+if __name__ == '__main__':
+    unittest.main()
+
