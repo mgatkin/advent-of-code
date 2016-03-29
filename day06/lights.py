9038c134c96011d27a28b341c89bcc147c22ec3d
Mark Atkin <mark@macbook.local>
2016-03-29 03:43:43 -0400

    Finally at least got it working!

--- day06/lights.py
+++ day06/lights.py
@@ -1,26 +1,53 @@
 #!/usr/bin/env python
 
 import fileinput
-import numpy as np
+import re
+import sys
+from os.path import basename
 
 def parse_instruction(s):
-    return s
+    r = re.search('(.*) ([0-9]+),([0-9]+) through ([0-9]+),([0-9]+)$', s)
+    return (r.group(1), [[int(r.group(2)),int(r.group(3))],[int(r.group(4)),int(r.group(5))]])
 
-def toggle(arr, m):
-    arr[m[0][0]:(m[1][0]+1),m[0][1]:(m[1][1]+1)] ^= np.ones((m[1][0]-m[0][0]+1,m[1][1]-m[0][1]+1),np.bool_)
+def set_light(arr, m, f):
+    xorg = m[0][0]
+    yorg = m[0][1]
+    xend = m[1][0]
+    yend = m[1][1]
+    deltax = xend - xorg + 1
+    deltay = yend - yorg + 1
+    for x in xrange(deltax):
+        for y in xrange(deltay):
+            arr[x + xorg][y + yorg] = f(arr[x + xorg][y + yorg])
     return arr
+    
+def toggle(arr, m):
+    return set_light(arr, m, lambda x: 1 - x)
 
 def turn_on(arr, m):
-    print arr, m
-    print m[0][0], m[1][0], m[0][1], m[1][1]
-    arr[m[0][0]:(m[1][0]+1),m[0][1]:(m[1][1]+1)] = np.ones((m[1][0]-m[0][0]+1,m[1][1]-m[0][1]+1),np.bool_)
-    return arr
+    return set_light(arr, m, lambda x: 1)
 
 def turn_off(arr, m):
-    arr[m[0][0]:(m[1][0]+1),m[0][1]:(m[1][1]+1)] = np.zeros((m[1][0]-m[0][0]+1,m[1][1]-m[0][1]+1),np.bool_)
-    return arr
+    return set_light(arr, m, lambda x: 0)
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print 'Usage:', basename(__file__), '<input file>'
+        sys.exit(0)
+
+    line_no = 0
+    lights = [[0 for x in xrange(1000)] for x in xrange(1000)] 
     for line in fileinput.input():
-        print line
+        line_no = line_no + 1
+        instruction, m = parse_instruction(line)
+        if instruction == 'toggle':
+            toggle(lights, m)
+        if instruction == 'turn on':
+            turn_on(lights, m)
+        if instruction == 'turn off':
+            turn_off(lights, m)
+    count = 0
+    for row, data in enumerate(lights):
+        count = count + sum(data)
+    print count, 'light(s) on'
 
--- day06/tests.py
+++ day06/tests.py
@@ -2,44 +2,50 @@
 
 import unittest
 
+import numpy as np
 from lights import parse_instruction, turn_on, turn_off, toggle
 
 class TestFunction(unittest.TestCase):
     def test_set_lights(self):
         self.assertEqual(turn_on(
             [ [ 0, 0, 0 ], [ 0, 0, 0 ], [ 0, 0, 0 ] ],
-            [ [ 1, 1 ], [ 1, 1 ] ]),
+            [ [ 1, 1 ], [ 2, 2 ] ]),
             [ [ 0, 0, 0 ], [ 0, 1, 1 ], [ 0, 1, 1 ] ])
 
         self.assertEqual(turn_on(
             [ [ 1, 1, 1 ], [ 1, 1, 1 ], [ 1, 1, 1 ] ],
-            [ [ 1, 1 ], [ 1, 1 ] ]),
+            [ [ 1, 1 ], [ 2, 2 ] ]),
             [ [ 1, 1, 1 ], [ 1, 1, 1 ], [ 1, 1, 1 ] ])
+
+        self.assertEqual(turn_on(
+            [ [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ] ],
+            [ [ 2, 2 ], [ 3, 3 ] ]),
+            [ [ 0, 0, 0, 0 ], [ 0, 0, 0, 0 ], [ 0, 0, 1, 1 ], [ 0, 0, 1, 1 ] ])
 
         self.assertEqual(turn_off(
             [ [ 0, 0, 0 ], [ 0, 0, 0 ], [ 0, 0, 0 ] ],
-            [ [ 1, 1 ], [ 1, 1 ] ]),
+            [ [ 1, 1 ], [ 2, 2 ] ]),
             [ [ 0, 0, 0 ], [ 0, 0, 0 ], [ 0, 0, 0 ] ])
 
         self.assertEqual(turn_off(
             [ [ 1, 1, 1 ], [ 1, 1, 1 ], [ 1, 1, 1 ] ],
-            [ [ 1, 1 ], [ 1, 1 ] ]),
+            [ [ 1, 1 ], [ 2, 2 ] ]),
             [ [ 1, 1, 1 ], [ 1, 0, 0 ], [ 1, 0, 0 ] ])
 
         self.assertEqual(toggle(
             [ [ 0, 0, 0 ], [ 0, 0, 0 ], [ 0, 0, 0 ] ],
-            [ [ 1, 1 ], [ 1, 1 ] ]),
+            [ [ 1, 1 ], [ 2, 2 ] ]),
             [ [ 0, 0, 0 ], [ 0, 1, 1 ], [ 0, 1, 1 ] ])
 
         self.assertEqual(toggle(
             [ [ 1, 1, 1 ], [ 1, 1, 1 ], [ 1, 1, 1 ] ],
-            [ [ 1, 1 ], [ 1, 1 ] ]),
+            [ [ 1, 1 ], [ 2, 2 ] ]),
             [ [ 1, 1, 1 ], [ 1, 0, 0 ], [ 1, 0, 0 ] ])
 
     def test_parsing(self):
-        self.assertEqual(parse_instruction('turn on 1,1 through 2,2'), 'turn on 1,1 through 2,2'),
-#        self.assertEqual(parse_instruction('turn on 1,1 through 2,2'),
-#            [ [ 0, 0, 0 ], [ 0, 1, 1 ], [ 0, 1, 1 ] ])
+        self.assertEqual(parse_instruction('turn on 1,1 through 2,2'), ('turn on', [[1,1],[2,2]])),
+        self.assertEqual(parse_instruction('turn off 11,12 through 13,14'), ('turn off', [[11,12],[13,14]])),
+        self.assertEqual(parse_instruction('toggle 1,2 through 3,4'), ('toggle', [[1,2],[3,4]])),
 
 if __name__ == '__main__':
     unittest.main()
