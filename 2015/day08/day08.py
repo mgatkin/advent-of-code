01795f455d9d8928bdafb69d350e92b359ce4ad8
Mark Atkin <mgatkin@gmail.com>
2017-12-15 16:26:59 -0500

    Part 2 for day 8 of 2015.

--- 2015/day08/day08.py
+++ 2015/day08/day08.py
@@ -25,18 +25,31 @@ def in_memory_length(d):
             backslash = True
         length += 1
     return length
-    
+
+def encoded_length(d):
+    length = 0
+    for c in d:
+        if c == '"' or c == '\\':
+            length += 1
+        length += 1
+    return length + 2
+
 if __name__ == '__main__':
     s = []
     all_literal_lengths = 0
     all_in_memory_lengths = 0
+    all_encoded_lengths = 0
     for n, line in enumerate(fileinput.input()):
         d = line.strip()
         s.append(d)
         all_literal_lengths += literal_length(d)
         all_in_memory_lengths += in_memory_length(d)
-        print 'literal length of', d, 'is ', literal_length(d)
-        print 'in-memory length of', d, 'is ', in_memory_length(d)
+        all_encoded_lengths += encoded_length(d)
+        print 'literal length of', d, 'is', literal_length(d)
+        print 'in-memory length of', d, 'is', in_memory_length(d)
+        print 'encoded length of', d, 'is', encoded_length(d)
     print 'sum of literal string lengths is', all_literal_lengths
     print 'sum of in-memory string lengths is', all_in_memory_lengths
-    print 'Difference is', all_literal_lengths - all_in_memory_lengths
+    print 'sum of encoded string lengths is', all_encoded_lengths
+    print 'Part 1 result is', all_literal_lengths - all_in_memory_lengths
+    print 'Part 2 result is', all_encoded_lengths - all_literal_lengths
