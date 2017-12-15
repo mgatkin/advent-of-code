cc4539186f42a149618a5944e246217b65380c02
Mark Atkin <mgatkin@gmail.com>
2017-12-15 15:35:17 -0500

    Final solution.

--- 2017/day02/day02.py
+++ 2017/day02/day02.py
@@ -6,12 +6,33 @@ def linediff(d):
     data = map(int, d.strip().split('\t'))
     return max(data) - min(data)
 
-def function2(d): 
-    return int(d)
+def checksum(d, f):
+    total = 0
+    for data in d.strip().split('\n'):
+        total += f(data)
+    return total
+
+def linediv(d): 
+    data = map(int, d.strip().split('\t'))
+    for n in data:
+        subset = list(data)
+        subset.remove(n)
+        for s in subset:
+            if n % s == 0:
+                return n / s
+    return 0
     
 if __name__ == '__main__':
     linediffs = []
+    lines = []
     for n, line in enumerate(fileinput.input()):
+        lines.append(line)
         linediffs.append(linediff(line))
         print 'linediff of line', n, 'is', linediffs[n]
-    print 'checksum is', sum(linediffs)
+    print 'checksum is', checksum(''.join(lines), linediff)
+
+    linedivs = []
+    for n, line in enumerate(fileinput.input()):
+        linedivs.append(linediv(line))
+        print 'linediv of line', n, 'is', linedivs[n]
+    print 'checksum is', checksum(''.join(lines), linediv)
--- 2017/day02/tests.py
+++ 2017/day02/tests.py
@@ -2,17 +2,20 @@
 
 import unittest
 
-from day02 import linediff, checksum
+from day02 import linediff, linediv, checksum
 
 class TestDay02(unittest.TestCase):
     def test_part_one(self):
         self.assertEqual(linediff('5\t1\t9\t5'), 8)
         self.assertEqual(linediff('7\t5\t3'), 4)
         self.assertEqual(linediff('2\t4\t6\t8'), 6)
-        #self.assertEqual(checksum('5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8'), 18)
+        self.assertEqual(checksum('5\t1\t9\t5\n7\t5\t3\n2\t4\t6\t8', linediff), 18)
 
-#    def test_part_two(self):
-#        self.assertEqual(function2('0'), 0)
+    def test_part_two(self):
+        self.assertEqual(linediv('5\t9\t2\t8'), 4)
+        self.assertEqual(linediv('9\t4\t7\t3'), 3)
+        self.assertEqual(linediv('3\t8\t6\t5'), 2)
+        self.assertEqual(checksum('5\t9\t2\t8\n9\t4\t7\t3\n3\t8\t6\t5', linediv), 9)
 
 if __name__ == '__main__':
     unittest.main()
