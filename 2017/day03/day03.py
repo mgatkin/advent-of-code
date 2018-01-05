801812aea4ade1e2ac98f144e94a220192ee5c1e
Mark Atkin <mgatkin@gmail.com>
2018-01-05 17:50:40 -0500

    Use "walking" logic instead.

--- 2017/day03/day03.py
+++ 2017/day03/day03.py
@@ -3,28 +3,46 @@
 from math import ceil, sqrt
 import fileinput
 
-def coords(n):
-    k = int(ceil((sqrt(n) - 1) / 2))
-    t = 2 * k + 1
-    m = t**2
-    t = t - 1
-    if n >= m - t:
-        return k - (m - n), -k
-    else:
-        m = m - t
-    if n >= m - t:
-        return -k, -k + (m - n)
-    else:
-        m = m - t
-    if n >= m - t:
-        return -k + (m - n), k
-    else:
-        return k, k - (m - n - t)
-
 def distance(n):
-    return abs(n[0]) + abs(n[1])
+    if n <= 1:
+        return 0
+    size = int(ceil((sqrt(n) - 1) / 2))
+    matrix = [[0 for x in range(2 * size + 1)] for y in range(2 * size + 1)]
+    x = size
+    y = size
+    direction = 4
+    for i in xrange(n):
+        matrix[y][x] = i + 1
+        if direction == 1:      # Right
+            if matrix[y - 1][x] == 0:
+                direction = 2
+                y = y - 1
+            else:
+                x = x + 1
+        elif direction == 2:    # Up
+            if matrix[y][x - 1] == 0:
+                direction = 3
+                x = x - 1
+            else:
+                y = y - 1
+        elif direction == 3:    # Left
+            if matrix[y + 1][x] == 0:
+                direction = 4
+                y = y + 1
+            else:
+                x = x - 1
+        else:                   # Down
+            if matrix[y][x + 1] == 0:
+                direction = 1
+                x = x + 1
+            else:
+                y = y + 1
+    for i in xrange(len(matrix)):
+        try:
+            return abs(i - size) + abs(matrix[i].index(n) - size)
+        except ValueError:
+            continue
 
 if __name__ == '__main__':
     for n, line in enumerate(fileinput.input()):
-        l = int(line.strip())
-        print 'Executing spiral on line', (n + 1), 'returned', coords(l), 'and', distance(coords(l))
+        print 'Executing spiral on line', (n + 1), 'returned', distance(int(line.strip()))
