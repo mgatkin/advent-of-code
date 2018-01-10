6c71d17feaba1f87e60d43203f3b61a12c55e654
Mark Atkin <mgatkin@gmail.com>
2018-01-10 14:28:04 -0500

    Day 10 for 2015 complete.  Extremely slow, but complete.

--- 2015/day10/day10.py
+++ 2015/day10/day10.py
@@ -9,16 +9,27 @@ def count_digits(d):
         next_digit = s[0]
         if len(s) > 1:
             pos = 1
-            while s[pos] == next_digit:
+            while pos < len(s) and s[pos] == next_digit:
                 pos = pos + 1
             result = result + chr(48 + pos)
             s = s[pos:]
         else:
             result = result + '1'
-            break
+            s = ''
         result = result + next_digit
     return result
     
+def process(n, d):
+    for i in xrange(n):
+        print 'Pass', (i + 1), '...'
+        d = count_digits(d)
+    return d
+
 if __name__ == '__main__':
     for n, line in enumerate(fileinput.input()):
-        print 'Executing count_digits on line', (n + 1), 'returned', count_digits(line)
+        try:
+            digits, count = line.strip().split()
+        except ValueError:
+            digits, count = line.strip(), 1
+        digits = process(int(count), digits)
+        print 'Processing line', (n + 1), 'returned', digits, '( length =', len(digits), ')'
