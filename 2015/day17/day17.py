245d25e063ff9ec4014f5f53aa4b9781ce39dfbf
Mark Atkin <mgatkin@gmail.com>
2018-01-24 09:21:50 -0500

    Part 2 of day 17 for 2015.

--- 2015/day17/day17.py
+++ 2015/day17/day17.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
 
+from sys import maxint
 from itertools import combinations
 import fileinput
 
 def number_of_combinations(data, total): 
-    number_of_combinations = 0
+    number_of_valid_combinations = 0
+    valid_combinations = []
     for n, i in enumerate(data):
         for c in combinations(data, n + 1):
             if sum(c) == total:
-                number_of_combinations = number_of_combinations + 1
-    return number_of_combinations 
+                number_of_valid_combinations = number_of_valid_combinations + 1
+                valid_combinations = valid_combinations + [ c ]
+    return number_of_valid_combinations, valid_combinations
+
+def minimum_containers(data):
+    minimum_number_of_containers = maxint
+    for n in data:
+        if len(n) < minimum_number_of_containers:
+            minimum_number_of_containers = len(n)
+    number_of_combinations = 0
+    for n in data:
+        if len(n) == minimum_number_of_containers:
+            number_of_combinations = number_of_combinations + 1
+    return minimum_number_of_containers, number_of_combinations
 
 if __name__ == '__main__':
     data = []
@@ -17,6 +31,12 @@ if __name__ == '__main__':
         data = data + [ int(line) ]
     count = [ 25, 150 ]
     for n in count:
-        print 'There are', number_of_combinations(data, n), 'combinations of containers for', n, 'liters of eggnog.'
+        (number_of_valid_combinations, valid_combinations) = number_of_combinations(data, n)
+        print 'There are', number_of_valid_combinations,
+        print 'combinations of containers for', n, 'liters of eggnog.'
+        (number_of_minimum_containers, number_of_minimum_container_combinations) = \
+                minimum_containers(valid_combinations)
+        print 'There are', number_of_minimum_container_combinations,
+        print 'combinations that use the minimum of', number_of_minimum_containers, 'containers.'
 
 
