ec9105269edc3df65d7519235528ab254a321f77
Mark Atkin <mgatkin@gmail.com>
2018-01-24 09:36:17 -0500

    Fix unit tests.

--- 2015/day17/tests.py
+++ 2015/day17/tests.py
@@ -2,12 +2,15 @@
 
 import unittest
 
-from day17 import number_of_combinations
+from day17 import number_of_combinations, minimum_containers
 
 class TestDay17(unittest.TestCase):
     def test_part_one(self):
         data = [ 20, 15, 10, 5, 5 ]
-        self.assertEqual(number_of_combinations(data, 25), 4)
+        (number, combinations) = number_of_combinations(data, 25)
+        self.assertEqual(number, 4)
+        (size, minimum) = minimum_containers(combinations)
+        self.assertEqual(minimum, 3)
 
 if __name__ == '__main__':
     unittest.main()
