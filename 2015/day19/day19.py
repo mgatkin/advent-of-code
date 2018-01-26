357cacb89708ff3f9fbc1ac18b67e9c4764546b8
Mark Atkin <mgatkin@gmail.com>
2018-01-26 15:38:47 -0500

    Completed Part 1 of day 19 for 2015.

--- 2015/day19/day19.py
+++ 2015/day19/day19.py
@@ -1,90 +1,40 @@
 #!/usr/bin/env python
 
+import re
 import argparse
 import fileinput
 
-def build_map(d, stuck = False):
-    data = []
-    for line in d:
-        data = data + [[ 1 if c == '#' else 0 for c in line ]]
-    if stuck:
-        end = len(data[0]) - 1
-        data[0][0] = 1
-        data[0][end] = 1
-        data[end][0] = 1
-        data[end][end] = 1
-    return data
-
-def count_neighbors(d, coords): 
-    (row, col) = coords
-    neighbors_on = 0
-    for c_row in xrange(3):
-        for c_col in xrange(3):
-            if c_row is not 1 or c_col is not 1:
-                neighbors_on = neighbors_on + is_neighbor_on(d, (row + c_row - 1, col + c_col - 1))
-    return neighbors_on
-
-def is_a_corner(coords, length):
-    (row, col) = coords
-    if (row is 0 and col is 0) or \
-            (row is 0 and col is length - 1) or \
-            (row is length - 1 and col is 0) or \
-            (row is length - 1 and col is length - 1):
-        return True
-    else:
-        return False
-
-def is_neighbor_on(d, coords):
-    (row, col) = coords
-    length = len(d[0])
-    if row < 0 or col < 0 or row >= length or col >= length:
-        return 0
-    else:
-        return d[row][col]
-
-def count_lights_on(d):
-    total = 0
-    for row in d:
-        total = total + sum(row)
-    return total
-
-def next_state(d, stuck = False):
-    new_d = []
-    for row, row_data in enumerate(d):
-        new_d = new_d + [[]]
-        for col in xrange(len(row_data)):
-            neighbors = count_neighbors(d, (row, col))
-            if stuck and is_a_corner((row, col), len(row_data)):
-                new_d[row] = new_d[row] + [ 1 ]
-            elif d[row][col] is 1:
-                if neighbors is 2 or neighbors is 3:
-                    new_d[row] = new_d[row] + [ 1 ]
+def process_substitutions(e, s):
+    possible_combinations = []
+    for item in s.iteritems():
+        for n, sub in enumerate(item[1]):
+            if verbose > 0:
+                print 'Processing substitution', n + 1
+            for m in re.finditer(item[0], e):
+                i = m.start()
+                if verbose > 1:
+                    print item[0], '=>', sub, '---', e, '=>', e[0:i] + '(' + sub + ')' + e[i + len(item[0]):],
+                new_combination = e[0:i] + sub + e[i + len(item[0]):]
+                if new_combination not in possible_combinations:
+                    if verbose > 1:
+                        print '(new molecule)'
+                    possible_combinations = possible_combinations + [ new_combination ]
+                    if verbose > 2:
+                        print new_combination
+                    if verbose > 3:
+                        print possible_combinations
                 else:
-                    new_d[row] = new_d[row] + [ 0 ]
-            else:
-                if neighbors is 3:
-                    new_d[row] = new_d[row] + [ 1 ]
-                else:
-                    new_d[row] = new_d[row] + [ 0 ]
-    return new_d
+                    if verbose > 1:
+                        print
+    if verbose > 2:
+        print possible_combinations
+    return possible_combinations
 
-def get_state(d):
-    data = []
-    for row in d:
-        data = data + [ ''.join([ '#' if c else '.' for c in row ]) ]
-    return data
-
-def print_state(d):
-    for row in d:
-        print ''.join([ '#' if c else '.' for c in row ])
-
-def unique_combinations(e, s):
-    return
-
-def process_molecules(s, m):
+def process_molecules(m, s):
     c = []
-    for e in m:
-        unique_combinations(e, s)
+    for n, e in enumerate(m):
+        p = process_substitutions(e, s)
+        c = c + [ p ]
     return c
 
 def build_tables(d):
@@ -102,30 +52,19 @@ def build_tables(d):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    #parser.add_argument('-c', '--count', default='1', help='Number of iterations')
-    #parser.add_argument('-s', '--stuck', action='store_true', help='Corners are stuck on')
     parser.add_argument('-v', '--verbose', action='count', help='Verbose output')
     parser.add_argument('files', metavar='FILE', nargs='*', help='files to read, if empty, stdin is used')
     args = parser.parse_args()
 
-    #count = int(args.count)
-    #stuck = args.stuck
     verbose = args.verbose
 
     data = []
     for line in fileinput.input(files=args.files):
         data = data + [ line.strip() ]
     substitutions, molecules = build_tables(data)
-    if verbose > 0:
+    if verbose > 2:
         print substitutions, molecules
-'''
-    for n in xrange(count):
-        data = next_state(data, stuck)
-        if verbose:
-            print_state(data)
-    count = count_lights_on(data)
-    if count == 1:
-        print '1 light is on'
-    else:
-        print count, 'lights are on'
-'''
+    results = process_molecules(molecules, substitutions)
+    for n, m in enumerate(results):
+        print 'Processing molecule', n + 1, 'results in', len(m), 'distince molecules.'
+
