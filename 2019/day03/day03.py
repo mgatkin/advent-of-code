fa2b646b81b398105a5811d2b5b33c0741d4ee8b
Mark Atkin <mgatkin@gmail.com>
2020-05-26 20:09:26 -0400

    Missed a couple of files in previous commit.

--- 2019/day03/day03.py
+++ 2019/day03/day03.py
@@ -1,42 +1,68 @@
 #!/usr/bin/env python
 
 import fileinput
+from sys import maxint
+
 
 def part1(data):
-    wire_directions = [ data[0].strip().split(','), data[1].strip().split(',') ]
+    wire_directions = [ data[0].split(','), data[1].split(',') ]
     wires = [ [ (0, 0) ], [ (0, 0) ] ]
-    for wire in wires:
-        for d in wire_directions:
-            current_pos = wire[len(wire) - 1]
-            direction = d[0]
-            length = int(d[0:])
-            for i in enumerate(length):
-                next_pos = find_next_position(current_pos, direction, length)
-                wire.append(next_pos)
-            current_pos = next_pos
+    for w, wire in enumerate(wires):
+        print 'Tracing wire', w
+        current_pos = wires[w][len(wires[w]) - 1]
+        for wire_direction in wire_directions[w]:
+            direction = wire_direction[0]
+            length = int(wire_direction[1:])
+            for i in xrange(length):
+                next_pos = find_next_position(current_pos, direction)
+                wires[w].append(next_pos)
+                current_pos = next_pos
+    intersections = []
+    print 'Finding intersections...'
+    length = len(wires[0][1:])
+    for i, w in enumerate(wires[0][1:]):
+        if i % 100 == 0:
+            print 'Checking', i, 'of', length
+        if w in wires[1][1:]:
+            intersections.append(w)
+    #for s in wires[0][1:]:
+    #    for d in wires[1][1:]:
+    #        if s == d:
+    #            intersections.append(s)
+    shortest_distance = maxint
+    closest_point = None
+    for i in intersections:
+        distance = abs(i[0]) + abs(i[1])
+        if distance < shortest_distance:
+            closest_point = i
+            shortest_distance = distance
+    if closest_point is not None:
+        print closest_point, shortest_distance
+    else:
+        print 'No intersection detected.'
 
-    for d in wire2:
-        print(d)
 
-def find_next_position(position, direction, length):
-    if direction == 'U':
-        adder = (0, -1)
-    elif direction == 'D':
+def find_next_position(position, direction):
+    if direction == 'R':
+        adder = (1, 0)
+    elif direction == 'U':
         adder = (0, 1)
     elif direction == 'L':
         adder = (-1, 0)
-    elif direction == 'R':
-        adder = (1, 0)
+    elif direction == 'D':
+        adder = (0, -1)
     else:
         adder = (0, 0)
-    return position
+    return add_tuple(position, adder)
 
-def function2(d): 
-    return d
+
+def add_tuple(a, b):
+    if len(a) == len(b) == 2:
+        return (a[0] + b[0], a[1] + b[1])
+
 
 if __name__ == '__main__':
     file_input = fileinput.input()
-    part1(file_input)
-    for n, line in enumerate(file_input):
-        print 'Executing function1 on line', (n + 1), 'returned', function1(line)
-        print 'Executing function2 on line', (n + 1), 'returned', function2(line)
+    lines = [ file_input.readline().strip(), file_input.readline().strip() ]
+    part1(lines)
+
